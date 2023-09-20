7cbe484caf43fdb9c44ae951a1911ab97d9a581c
Sarah <Sarah@HP-SARAHI>
2023-09-20 13:20:53 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack stack = new Stack(5);
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("Element at the top of the stack: " + stack.Peek());
+
+            Console.WriteLine("Popping elements:");
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private int maxSize;
+        private int top;
+        private int[] stackArray;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new int[maxSize];
+            top = -1;
+        }
+
+        public void Push(int value)
+        {
+            if (top < maxSize - 1)
+            {
+                stackArray[++top] = value;
+            }
+            else
+            {
+                Console.WriteLine("The stack is full. You cannot add more elements.");
+            }
+        }
+
+        public int Pop()
+        {
+            if (top >= 0)
+            {
+                return stackArray[top--];
+            }
+            else
+            {
+                Console.WriteLine("The stack is empty. You cannot extract any element.");
+                return -1; // Return value to indicate an error
+            }
+        }
+
+        public int Peek()
+        {
+            if (top >= 0)
+            {
+                return stackArray[top];
+            }
+            else
+            {
+                Console.WriteLine("The stack is empty. You cannot peek at any element.");
+                return -1; // Return value to indicate an error
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public int Size()
+        {
+            return top + 1;
+        }
+    }
+}
